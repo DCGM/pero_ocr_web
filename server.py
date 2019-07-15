cbfc791a4b91eaeb29645d5b7d9cdb14fd86b88f
David Endrych <xendry02@vutbr.cz>
2019-07-15 13:37:59 +0200

    Move route files to folder routes

--- server.py
+++ server.py
@@ -24,11 +24,11 @@ def create_app():
         return User.query.get(int(user_id))
 
     # blueprint for auth routes in our app
-    from auth import auth as auth_blueprint
+    from routes.auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     # blueprint for non-auth parts of app
-    from main import main as main_blueprint
+    from routes.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
