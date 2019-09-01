b3ded0cd93de649c0bac4d45f1ead99f7152146d
David Endrych <david.endrych@gmail.com>
2019-09-01 16:51:03 +0200

    Add Url prefix in auth routes

--- app/__init__.py
+++ app/__init__.py
@@ -34,7 +34,7 @@ def create_app():
     app.register_blueprint(main_bp)
 
     from app.auth import bp as auth_bp
-    app.register_blueprint(auth_bp)
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
     from app.document import bp as document_bp
     app.register_blueprint(document_bp, url_prefix='/document')
