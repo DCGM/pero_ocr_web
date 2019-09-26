35aede1411fba6bda0ac20493a86480e549a6b64
David Endrych <david.endrych@gmail.com>
2019-09-26 22:55:11 +0200

    Fix allowed extensions for uploading files

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ DEBUG = True
 UPLOAD_IMAGE_FOLDER = '/home/ikohut/projects_new/pero_ocr_web_data/uploaded_images/'
 LAYOUT_RESULTS_FOLDER = '/home/ikohut/projects_new/pero_ocr_web_data/layout_results/'
 OCR_RESULTS_FOLDER = '/home/ikohut/projects_new/pero_ocr_web_data/ocr_results/'
-EXTENSIONS = ('jpg', 'png', 'pdf')
+EXTENSIONS = ('jpg', 'png', 'jpeg')
