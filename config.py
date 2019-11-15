5e1854e21dd8a4ec00fac45019999af2d2683435
Jan Kohut <kohut.honza@seznam.cz>
2019-11-15 16:11:43 +0100

    Optimized json transfer.

--- config.py
+++ config.py
@@ -2,11 +2,13 @@
 database_url = 'sqlite:////mnt/data/pero_ocr_web_data/db.sqlite'
 
 class Config(object):
-    DEBUG = True
+    DEBUG = False
     UPLOAD_IMAGE_FOLDER = '/mnt/data/pero_ocr_web_data/uploaded_images/'
     LAYOUT_RESULTS_FOLDER = '/mnt/data/pero_ocr_web_data/layout_analysis/layout_results/'
     OCR_RESULTS_FOLDER = '/mnt/data/pero_ocr_web_data/ocr/ocr_results/'
     MODELS_FOLDER = '/mnt/data/pero_ocr_web_data/models'
     EXTENSIONS = ('jpg', 'png', 'pdf', 'jpeg')
     SECRET_KEY = '35q0HKGItx35FvnC4G3uUrXXXzH8RBZ3'
+    JSONIFY_PRETTYPRINT_REGULAR = False
+    JSON_SORT_KEYS = False
 
