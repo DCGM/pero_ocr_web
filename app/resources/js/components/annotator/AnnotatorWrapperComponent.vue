f0d0807d78b0db4cfa6c22db421cd797a6ed350d
David Hříbek <davidhribek@seznam.cz>
2021-08-18 15:20:05 +0200

    refactor

--- app/resources/js/components/annotator/AnnotatorWrapperComponent.vue
+++ app/resources/js/components/annotator/AnnotatorWrapperComponent.vue
@@ -79,47 +79,10 @@ export default {
 
 
 
-        // Custom event handler
-        myEventHandler: (type, event) => {
-        //     console.log('selected event ' + type, event);
-        },
 
         annotationDeletedEventHandler(type, uuid) {
             axios.delete('/api/annotations/' + type + '/' + uuid);
         },
-
-        imageSelectedEventHandler(image_id) {
-            return new Promise((resolve, reject) => {
-                this.fetchImageWithAnnotationsPromise(image_id, this.dataset_id)
-                    .then((dataset_image) => {
-                        // Select image
-                        this.load_image(dataset_image.image.path);
-
-                        // // Load region annotations
-                        // this.load_annotations(dataset_image.region_annotations);
-                        // // Load row annotations
-                        // this.$refs.annotator_component.loadAnnotations(dataset_image.row_annotations);
-                        resolve();
-                    });
-            });
-        },
-        fetchDatasetWithImagesPromise(dataset_id) {
-            return new Promise(function (resolve, reject) {
-                axios
-                    .get('/api/datasets/showWithImages/' + dataset_id)
-                    .then((response) => resolve(response.data))
-                    .catch((errors) => reject(errors));
-            });
-        },
-        fetchImageWithAnnotationsPromise(image_id, dataset_id) {
-            return new Promise(function (resolve, reject) {
-                axios
-                    .get('/api/datasetImageWithAnnotations/' + dataset_id + '/image/' + image_id)
-                    .then((response) => resolve(response.data))
-                    .catch((errors) => reject(errors));
-            });
-        },
-
         annotationCreatedEditedEventHandler(annotation, annotation_type, image_id=null) {
             axios
                 .post('/api/annotations', {
@@ -132,36 +95,6 @@ export default {
         }
     },
     mounted() {
-      // this.image_id = 1;
-        // Get image and dataset id from url
-        // let url_params = window.location.pathname.split('/');
-        // this.dataset_id = url_params[3];
-        // this.image_id = url_params.length >= 5? url_params[4]: null;
-        // let row_uuid = url_params.length >= 6? url_params[5]: null;
-        // console.log(url_params);
-        // console.log(this.document_id)
-
-        // this.fetchDatasetWithImagesPromise(this.dataset_id)
-        //     .then((dataset) => {
-                // Load dataset to annotator
-                // this.$refs.annotator_component.canvasSelectDataset(dataset);  // Optional
-
-                // Load image with annotations to annotator
-                // let selected_image = dataset.images.find((item) => item.id === this.image_id);
-                // this.image_id = selected_image? selected_image.id: dataset.images[0].id;  // Find or first
-
-                // this.imageSelectedEventHandler(this.image_id)
-                //     .then(() => {
-                //         // Select row annotation
-                //         if (row_uuid)
-                //             this.$refs.annotator_component.canvasSelectRowAnnotation(row_uuid);
-                //     });
-            // });
-
-        // this.$on('imageSelectEv_imageSelected', (image_id) => {
-        //     this.image_id = image_id;
-        //     this.imageSelectedEventHandler(image_id);
-        // });
     }
 }
 </script>
