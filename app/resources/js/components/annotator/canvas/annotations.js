cacdc06addad4d1936386316ddba66e38790e9bf
David Hříbek <davidhribek@seznam.cz>
2021-09-02 15:02:00 +0200

    remove row text view

--- app/resources/js/components/annotator/canvas/annotations.js
+++ app/resources/js/components/annotator/canvas/annotations.js
@@ -68,7 +68,6 @@ export function serializeAnnotation(annotation) {
     // Serialize view
     copy.points = getPathPoints(copy.view.path);
     if (annotation.hasOwnProperty('text')) {  // Row annotation
-        copy.text = copy.view.text.content;
         copy.baseline = getPathPoints(copy.view.baseline.baseline_path);
         copy.heights = [
             copy.view.baseline.baseline_left_path.segments[1].point.subtract(copy.view.baseline.baseline_path.segments[0].point).length,
