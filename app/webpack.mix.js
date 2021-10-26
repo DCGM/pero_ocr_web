1ddf528f78a000299b1c469a8c018906f247440d
David Hříbek <davidhribek@seznam.cz>
2021-10-26 09:47:12 +0200

    comments, refactor

--- app/webpack.mix.js
+++ app/webpack.mix.js
@@ -3,5 +3,5 @@ let mix = require('laravel-mix');
 
 mix.setPublicPath('static');
 
-mix.js('resources/js/app.js', '')
-   .sass('resources/sass/app.scss', '');
+mix.js('resources/js/app.js', 'annotator_component.js')
+   .sass('resources/sass/app.scss', 'annotator_component.css');
