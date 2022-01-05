bac14bf7d7eac25338d65a6f8542475e4f22f6d0
Michal Hradiš <michal.hradis@gmail.com>
2022-01-05 15:01:57 +0100

    SQL script which creates DB triggers.

---
+++ sql/add_database_triggers.sql
@@ -0,0 +1,87 @@
+CREATE VIEW doc_ann_line_counts 
+AS
+SELECT documents.id, COUNT(*) AS line_count
+FROM documents, images, textregions, textlines
+WHERE documents.id = images.document_id and images.id = textregions.image_id and textregions.id = textlines.region_id and textlines.annotated
+GROUP BY documents.id;
+
+CREATE VIEW doc_line_counts 
+AS
+SELECT documents.id, COUNT(*) AS line_count
+FROM documents, images, textregions, textlines
+WHERE documents.id = images.document_id and images.id = textregions.image_id and textregions.id = textlines.region_id
+GROUP BY documents.id;
+
+CREATE OR REPLACE FUNCTION update_annotated_insert()
+RETURNS TRIGGER AS
+'
+BEGIN
+    UPDATE textlines
+        SET annotated = true
+        WHERE id = NEW.text_line_id;
+    RETURN NEW;
+END
+' LANGUAGE plpgsql;
+
+CREATE TRIGGER annotations_update_annotated_insert AFTER INSERT ON annotations
+    FOR EACH ROW EXECUTE PROCEDURE update_annotated_insert();
+
+CREATE OR REPLACE FUNCTION update_document_annotated()
+RETURNS TRIGGER AS
+'
+DECLARE
+    del_update integer := 0;
+    ann_update integer := 0;
+
+BEGIN
+	if(OLD.annotated != NEW.annotated) then
+		UPDATE documents
+			SET annotated_line_count = annotated_line_count + 1
+			from images, textregions
+			where documents.id = images.document_id and images.id = textregions.image_id and textregions.id = NEW.region_id;
+	end if;
+	if(OLD.deleted != NEW.deleted) then
+		if(NEW.annotated) then
+			ann_update = 1;
+		end if;
+		if(NEW.deleted) then
+			del_update := -1;
+			ann_update := ann_update * -1;
+		else
+			del_update := +1;
+		end if;
+		UPDATE documents
+			SET 
+				annotated_line_count = annotated_line_count + ann_update,
+				line_count = line_count + del_update
+			from images, textregions
+			where documents.id = images.document_id and images.id = textregions.image_id and textregions.id = NEW.region_id;
+	end if;
+    RETURN NEW;
+END
+' LANGUAGE plpgsql;
+
+CREATE TRIGGER textlines_update_document_annotated AFTER UPDATE ON textlines
+    FOR EACH ROW EXECUTE PROCEDURE update_document_annotated();
+
+
+CREATE OR REPLACE FUNCTION insert_document_line()
+RETURNS TRIGGER AS
+'
+DECLARE
+    del_update integer := 0;
+    ann_update integer := 0;
+
+BEGIN
+	UPDATE documents
+	SET line_count = line_count + 1
+	from images, textregions
+	where documents.id = images.document_id and images.id = textregions.image_id and textregions.id = NEW.region_id;
+    RETURN NEW;
+END
+' LANGUAGE plpgsql;
+
+CREATE TRIGGER textlines_insert_document_line AFTER INSERT ON textlines
+    FOR EACH ROW EXECUTE PROCEDURE insert_document_line();
+
+
