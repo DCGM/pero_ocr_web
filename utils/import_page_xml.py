fbf136cee4e1ff10900775c400330662156aa27d
Stefan Weil <sw@weilnetz.de>
2022-06-06 19:10:56 +0200

    Remove unnecessary 'pass' statement (reported by LGTM)
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- utils/import_page_xml.py
+++ utils/import_page_xml.py
@@ -120,7 +120,6 @@ def main():
                     line.transcription = ''
                     confidences = []
                 else:
-                    pass
                     line.transcription, confidences = apply_rules(line.transcription, replace_rules, weird_characters)
 
                 try:
@@ -146,6 +145,3 @@ def main():
 
 if __name__ == '__main__':
     sys.exit(main())
-
-
-
