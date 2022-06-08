a2f18846d39d80f84c82cc21bbcabc8e190f5f5a
Jan Kohut <kohut.honza@seznam.cz>
2022-06-08 13:27:35 +0200

    Add request to routes.

--- app/__init__.py
+++ app/__init__.py
@@ -56,7 +56,7 @@ def create_app():
     from app.document import bp as document_bp
     app.register_blueprint(document_bp, url_prefix='/document')
 
-    from app.document import bp as request_bp
+    from app.request import bp as request_bp
     app.register_blueprint(request_bp, url_prefix='/request')
 
     from app.profile import bp as profile_bp
